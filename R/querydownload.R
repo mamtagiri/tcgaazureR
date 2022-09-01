8b754d1b54b2ed502d917876aedfa17c9be1abb8
Mamta Giri <44902002+mamtagiri@users.noreply.github.com>
2022-09-01 09:12:24 -0400

    Update querydownload.R

--- R/querydownload.R
+++ R/querydownload.R
@@ -2,7 +2,7 @@ require(AzureStor)
 querydownload<-function(data,destination){
   t<-AzureStor:::blob_container("https://storagepurviewmg.blob.core.windows.net/testdata1/",sas ="sp=rl&st=2022-07-25T19:47:41Z&se=2022-10-01T03:47:41Z&spr=https&sv=2021-06-08&sr=c&sig=TVUhb4T%2Fk%2BNW5VO2Ej21lZyqkBtGBdamn0kaYX3%2BahY%3D")
   files <- file.path(
-    ret$project, "harmonized",
+    data$project, "harmonized",
     gsub(" ","_",data$data.category),
     gsub(" ","_",data$data.type))
   files <- file.path(destination, files)
@@ -10,10 +10,10 @@ querydownload<-function(data,destination){
   setwd(files)
   for(i in 1:nrow(data$results[[1]])) {
     current<-getwd()
-    dir<-ret$results[[1]][i,1]
+    dir<-data$results[[1]][i,1]
     dir.create(dir)
     setwd(dir)
-    file=file.path(ret$results[[1]][i,1],ret$results[[1]][i,5])
+    file=file.path(data$results[[1]][i,1],data$results[[1]][i,5])
     download_blob(t, file)
     setwd(current)
   }
